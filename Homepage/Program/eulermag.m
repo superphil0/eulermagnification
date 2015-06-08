68579f353b61e2daa5ecc6021084045c2c9a360f
getshi <getshi@gmx.at>
2015-06-08 13:59:29 +0200

    start code doku

---
+++ Homepage/Program/eulermag.m
@@ -0,0 +1,86 @@
+function eulermag ( filein, fileout, alpha, lambdaC, lowCutoff, highCutoff, chromAtt, exaggerationFactor, maxframes ) 
+% Reads the input video file, awaits user input for magnification area 
+% selection, then converts the frames to YIC color space applies
+% motion magnification and writes the output video.
+%
+% Input Parameters
+%   filein              see lpiir.m
+%   fileout             see lpiir.m
+%   alpha               see lpiir.m
+%   lambdaC             see lpiir.m
+%   lowCutoff           see lpiir.m
+%   highCutoff          see lpiir.m
+%   chromAtt            see lpiir.m
+%   exaggerationFactor  see lpiir.m
+%   maxframes           Maximum number of frames to be used or 0 if all
+
+% Open video reader
+vidIn = VideoReader(filein);
+startIndex = 1;
+
+% Set endIndex
+if (maxframes > 0)
+    endIndex = maxframes;
+else
+    endIndex = vidIn.NumberOfFrames;
+end
+
+% Video file properties
+nChannels = 3;
+temp = struct('cdata', ...
+		  zeros(vidIn.Height, vidIn.Width, nChannels, 'uint8'), ...
+		  'colormap', []);
+
+% Frame storage
+frames = zeros(vidIn.Height, vidIn.Width, nChannels, endIndex);
+
+% Read and show first frame
+temp.cdata = read(vidIn, 1);
+[rgbframe,~] = frame2im(temp);
+imshow(rgbframe)
+
+% Get rectangular area for motion magnification
+rect = uint16(getrect);
+
+% Read and convert all frames to YIC color space
+for i=startIndex:endIndex
+    % Read frame
+    temp.cdata = read(vidIn, i);
+    [rgbframe,~] = frame2im(temp);
+    rgbframe = im2double(rgbframe);
+    % Convert to YIC
+    frame = rgb2ntsc(rgbframe);
+    frames(:,:,:,i) = frame;
+end
+
+%% Apply eulerian motion magnification
+% with temporal IIR filter and spatial Laplacian pyramid
+% on the user-defined magnification area
+
+magnifyArea = frames(rect(2): rect(2)+rect(4),rect(1):rect(1)+rect(3),:,:);
+framesOut = lpiir(magnifyArea, alpha, lambdaC, lowCutoff, highCutoff, chromAtt, exaggerationFactor);
+
+%% Write output
+vidOut = VideoWriter(fileout);
+vidOut.FrameRate = vidIn.FrameRate;
+
+open(vidOut)
+
+% Convert each frame back to RGB and write to file
+for i=startIndex:endIndex
+    % Convert YIC to RGB color space
+    rgbframe = ntsc2rgb(framesOut(:,:,:,i));
+    progmeter(i,endIndex);
+    % Clamp values to [0,1]
+    rgbframe(rgbframe < 0) = 0;
+    rgbframe(rgbframe > 1) = 1;
+    % Add magnification area output back to whole frame
+    fr = ntsc2rgb(frames(:,:,:,i));
+    fr(rect(2): rect(2)+rect(4),rect(1):rect(1)+rect(3),:)=rgbframe;
+    rgbframe = fr;
+    writeVideo(vidOut, rgbframe);
+end
+close(vidOut);
+disp('wrote video file');
+
+end
--- Homepage/Program/main.m
+++ Homepage/Program/main.m
@@ -1,60 +1,15 @@
-%% params
-alpha = 10;
-lowCutoff = 0.4;
-highCutoff = 0.05;
-lambdaC = 16;
-chromAtt = 0.1;
-filename = 'wrist.mp4';
-exaggerationFactor = 8;
 
-%% read file
+%% Specify the parameters
+filein = 'wrist.mp4';   % Input file
+fileout = 'out';        % Output file
+alpha = 10;             % ?
+lowCutoff = 0.4;        % Low cutoff
+highCutoff = 0.05;      % High cutoff
+lambdaC = 16;           % ?
+chromAtt = 0.1;         % Chromattic Attenuation
+exaggerationFactor = 8; % Motion exaggeration factor
+maxframes = 0;          % Maximum number of frames used from the video
 
-vidIn = VideoReader(filename);
-startIndex = 1;
-endIndex = vidIn.NumberOfFrames;
-nChannels = 3;
-temp = struct('cdata', ...
-		  zeros(vidIn.Height, vidIn.Width, nChannels, 'uint8'), ...
-		  'colormap', []);
-      
-frames = zeros(vidIn.Height, vidIn.Width, nChannels, endIndex);
-
-temp.cdata = read(vidIn, 1);
-[rgbframe,~] = frame2im(temp);
-imshow(rgbframe)
-rect = uint16(getrect);
-%%
-for i=startIndex:endIndex
-    temp.cdata = read(vidIn, i);
-    [rgbframe,~] = frame2im(temp);
-    rgbframe = im2double(rgbframe);
-    % Convert RGB to YIC color space
-    frame = rgb2ntsc(rgbframe);
-    frames(:,:,:,i) = frame;
-end
-
-%% magnify motion (all operations work on YIC-double image pyramid frames)
-magnifyArea = frames(rect(2): rect(2)+rect(4),rect(1):rect(1)+rect(3),:,:);
-framesOut = lpiir(magnifyArea, alpha, lambdaC, lowCutoff, highCutoff, chromAtt, exaggerationFactor);
-
-%% save video file
-outName = 'out';
-vidOut = VideoWriter(outName);
-vidOut.FrameRate = vidIn.FrameRate;
-
-open(vidOut)
-
-for i=startIndex:endIndex
-    % Convert YIC to RGB color space
-    rgbframe = ntsc2rgb(framesOut(:,:,:,i));
-    progmeter(i,endIndex);
-    % Clamp values to [0,1]
-    rgbframe(rgbframe < 0) = 0;
-    rgbframe(rgbframe > 1) = 1;
-    fr = ntsc2rgb(frames(:,:,:,i));
-    fr(rect(2): rect(2)+rect(4),rect(1):rect(1)+rect(3),:)=rgbframe;
-    rgbframe = fr;
-    writeVideo(vidOut, rgbframe);
-end
-close(vidOut);
-disp('wrote video file');
+%% Perform Eulerian motion magnification
+eulermag ( filein, fileout, alpha, lambdaC, lowCutoff, highCutoff, ...
+    chromAtt, exaggerationFactor, maxframes);

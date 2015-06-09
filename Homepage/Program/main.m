2c4049702a73794ecfcf1bfd9a52d2cbcc48ef22
Philipp Omenitsch <philippomenitsch@gmx.at>
2015-06-09 13:44:38 +0200

    changed index

--- Homepage/Program/main.m
+++ Homepage/Program/main.m
@@ -1,13 +1,13 @@
 
 %% Specify the parameters
-filein = 'orig_guitar.mp4';   % Input file
+filein = 'wrist.mp4';   % Input file
 fileout = 'out';        % Output file
-alpha = 80;             % Amplification factor
-lowCutoff = 0.1;        % Temporal frequency low cutoff
-highCutoff = 0.01;      % Temporal frequency high cutoff
+alpha = 200;             % Amplification factor
+lowCutoff = 1.2;        % Temporal frequency low cutoff
+highCutoff = 0.4;      % Temporal frequency high cutoff
 lambdaC = 16;           % Spatial frequency cutoff
 chromAtt = 0.1;         % Chromattic Attenuation
-exaggerationFactor = 22; % Motion exaggeration factor
+exaggerationFactor = 140; % Motion exaggeration factor
 maxframes = 0;          % Maximum number of frames used from the video
 
 %% Perform Eulerian motion magnification
